404902c148070856642bba9b2cf57a8803a29499
Isabela Duarte <isabeladuartelbg@gmail.com>
2022-06-22 22:26:24 -0300

    API lessons the graphCMS and import data-fns

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,25 +1,47 @@
-import { CheckCircle } from 'phosphor-react'
+import { CheckCircle, Lock } from 'phosphor-react'
+import {isPast, format} from 'date-fns' 
+import ptBR from 'date-fns/locale/pt-BR'
 
-export function Lesson() {
+interface LessonProps {
+    title: string;
+    slug: string;
+    availableAt: Date;
+    type: 'live' | 'class';
+}
+
+export function Lesson(props: LessonProps) {
+    const isLessonAvailable = isPast(props.availableAt) // Função isPast verifica se a data já passou
+    const availableDateFormatted = format(props.availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm", {
+        locale: ptBR,
+    })
+
     return (
         <a href="#">
             <span className="text-brand-gray-300">
-                Terça • 22 de junho • 19h00
+                {availableDateFormatted}
             </span>
 
             <div className="rounded border border-brand-gray-500 p-4 mt-2">
                 <header className="flex items-center justify-between">
-                    <span className="text-sm text-brand-blue-500 flex items-center gap-2">
-                        <CheckCircle size={20} />
-                        Conteúdo liberado
-                    </span>
+                    {isLessonAvailable ? (
+                        <span className="text-sm text-brand-blue-500 flex items-center gap-2">
+                            <CheckCircle size={20} />
+                            Conteúdo liberado
+                        </span>
+                    ) : (
+                        <span className="text-sm text-brand-orange-500 flex items-center gap-2">
+                            <Lock size={20} />
+                            Em breve
+                        </span>
+                    )}
+
                     <span className="text-xs rounded py=[0.125rem] px-2 text-white border border-brand-green-300">
-                        AO VIVO
+                        {props.type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
                     </span>
                 </header>
 
                 <strong className="text-brand-gray-200 mt-5 block">
-                    Abertura do evento Ignite lab
+                    {props.title}
                 </strong>
             </div>
         </a>

6977cec8cde41d09984217698c130875689b0093
Isabela Duarte <isabeladuartelbg@gmail.com>
2022-06-22 21:37:17 -0300

    Create lessons, styling and import phosphor

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,5 +1,27 @@
+import { CheckCircle } from 'phosphor-react'
+
 export function Lesson() {
     return (
-        <h1>Lesson</h1>
+        <a href="#">
+            <span className="text-brand-gray-300">
+                Terça • 22 de junho • 19h00
+            </span>
+
+            <div className="rounded border border-brand-gray-500 p-4 mt-2">
+                <header className="flex items-center justify-between">
+                    <span className="text-sm text-brand-blue-500 flex items-center gap-2">
+                        <CheckCircle size={20} />
+                        Conteúdo liberado
+                    </span>
+                    <span className="text-xs rounded py=[0.125rem] px-2 text-white border border-brand-green-300">
+                        AO VIVO
+                    </span>
+                </header>
+
+                <strong className="text-brand-gray-200 mt-5 block">
+                    Abertura do evento Ignite lab
+                </strong>
+            </div>
+        </a>
     )
 }

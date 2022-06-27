991d3e7da816d11939ee9e29ffd4cb1dda14e4f8
Isabela Duarte <isabeladuartelbg@gmail.com>
2022-06-27 12:42:16 -0300

    Add image background image, form and button submit

---
+++ src/pages/Subscribe.tsx
@@ -0,0 +1,42 @@
+import { Logo } from "../components/Logo";
+
+export function Subscribe () {
+    return (
+        <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
+            <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
+                <div className="max-w-[640px]">
+                   <Logo /> 
+                   <h1 className="mt-8 text-[2.5rem] leading-tight">
+                        Construa uma <strong className="text-brand-blue-500">aplicação completa</strong>, do zero, com <strong className="text-brand-blue-500">React</strong>
+                   </h1>
+                   <p className="mt-4 text-brand-gray-200">
+                        Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.
+                   </p>
+                </div>
+                <div className="p-8 bg-brand-gray-700 border border-brand-gray-500 rounded">
+                    <strong className="text-2xl mb-6 block">Inscreva-se gratuitamente</strong>
+                    <form action="" className="flex flex-col gap-2 w-full">
+                        <input 
+                            className="bg-brand-gray-900 rounded px-5 h-14"
+                            type="text" 
+                            placeholder="Seu nome completo" id="" 
+                        />
+                        <input 
+                            className="bg-brand-gray-900 rounded px-5 h-14"
+                            type="email" 
+                            placeholder="Digite seu e-mail" id="" 
+                        />
+
+                        <button 
+                            type="submit"
+                            className="mt-4 bg-brand-green-500 uppercase py-5 rounded font-bold text-sm hover:bg-brand-green-700 transition-colors"
+                        >
+                            Garantir minha vaga
+                        </button>
+                    </form>
+                </div>
+            </div>
+            <img src="/src/assets/code-mockup.png" alt="" />
+        </div>
+    )
+}
